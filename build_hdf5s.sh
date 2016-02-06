5b185d4ef9a146d26a2912fd388dc7487f9e41c4
Matthew Brett <matthew.brett@gmail.com>
2016-02-06 02:15:27 -0800

    Script to build hdf5 archives

---
+++ build_hdf5s.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Build libh5py library
+set -e
+
+SZIP_VERSION=2.1
+SZIP_URL=http://www.hdfgroup.org/ftp/lib-external/szip/
+HDF5_VERSIONS="1.8.3 1.8.18"
+HDF5_URL=http://www.hdfgroup.org/ftp/HDF5/releases
+
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+yum install -y zlib-devel
+curl -sLO $SZIP_URL/$SZIP_VERSION/src/szip-$SZIP_VERSION.tar.gz
+tar zxf szip-$SZIP_VERSION.tar.gz
+( cd szip-$SZIP_VERSION && ./configure --enable-encoding=no --prefix=/usr/local && make && make install )
+
+for HDF5_VERSION in ${HDF5_VERSIONS}; do
+    curl -sLO $HDF5_URL/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz
+    tar zxf hdf5-$HDF5_VERSION.tar.gz
+    ( cd hdf5-$HDF5_VERSION && ./configure --prefix=/usr/local --with-szlib=/usr/local && make && make install )
+    tar zcf /io/hdf5-$HDF5_VERSION.tgz /usr/local/lib /usr/local/bin/*h5* /usr/local/include /usr/local/hdf5*
+done
