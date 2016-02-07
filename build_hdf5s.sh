a80c890d480cffb04bd599a75e765c2da9e3ed13
Matthew Brett <matthew.brett@gmail.com>
2016-02-06 17:02:29 -0800

    Fix build of hdf5 libraries
    I was building one on top of the other; remove one before building the next.

--- build_hdf5s.sh
+++ build_hdf5s.sh
@@ -10,13 +10,14 @@ HDF5_URL=http://www.hdfgroup.org/ftp/HDF5/releases
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
 yum install -y zlib-devel
-curl -sLO $SZIP_URL/$SZIP_VERSION/src/szip-$SZIP_VERSION.tar.gz
-tar zxf szip-$SZIP_VERSION.tar.gz
-( cd szip-$SZIP_VERSION && ./configure --enable-encoding=no --prefix=/usr/local && make && make install )
 
 for HDF5_VERSION in ${HDF5_VERSIONS}; do
+    curl -sLO $SZIP_URL/$SZIP_VERSION/src/szip-$SZIP_VERSION.tar.gz
+    tar zxf szip-$SZIP_VERSION.tar.gz
+    ( cd szip-$SZIP_VERSION && ./configure --enable-encoding=no --prefix=/usr/local && make && make install )
     curl -sLO $HDF5_URL/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz
     tar zxf hdf5-$HDF5_VERSION.tar.gz
     ( cd hdf5-$HDF5_VERSION && ./configure --prefix=/usr/local --with-szlib=/usr/local && make && make install )
-    tar zcf /io/hdf5-$HDF5_VERSION.tgz /usr/local/lib /usr/local/bin/*h5* /usr/local/include /usr/local/hdf5*
+    tar zcf /io/hdf5-$HDF5_VERSION.tgz /usr/local/lib/* /usr/local/bin/*h5* /usr/local/include/* /usr/local/hdf5*
+    rm -rf /usr/local/lib/* /usr/local/bin/*h5* /usr/local/include/* /usr/local/hdf5*
 done
