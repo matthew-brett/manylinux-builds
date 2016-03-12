4fcecdfbcba2a6be4b1747a11e36d5bfa871ee89
Matthew Brett <matthew.brett@gmail.com>
2016-03-12 14:50:36 -0800

    Fix HDF5 builds, maybe

--- build_hdf5s.sh
+++ build_hdf5s.sh
@@ -11,8 +11,9 @@ set -e
 source /io/common_vars.sh
 
 if [ -z "$HDF5_VERSIONS" ]; then
-    HDF5_VERSIONS="1.8.3 1.8.18"
+    HDF5_VERSIONS="1.8.3 1.8.16"
 fi
+echo "Building $HDF5_VERSIONS"
 
 SZIP_VERSION=2.1
 SZIP_URL=http://www.hdfgroup.org/ftp/lib-external/szip/
@@ -20,13 +21,19 @@ HDF5_URL=http://www.hdfgroup.org/ftp/HDF5/releases
 
 yum install -y zlib-devel
 
+# Build szip
+curl -sLO $SZIP_URL/$SZIP_VERSION/src/szip-$SZIP_VERSION.tar.gz
+tar zxf szip-$SZIP_VERSION.tar.gz
+( cd szip-$SZIP_VERSION && ./configure --enable-encoding=no --prefix=/usr/local && make )
+
 for HDF5_VERSION in ${HDF5_VERSIONS}; do
-    curl -sLO $SZIP_URL/$SZIP_VERSION/src/szip-$SZIP_VERSION.tar.gz
-    tar zxf szip-$SZIP_VERSION.tar.gz
-    ( cd szip-$SZIP_VERSION && ./configure --enable-encoding=no --prefix=/usr/local && make && make install )
+    # Install szip
+    ( cd szip-$SZIP_VERSION && make install )
+    # Build and install hdf5
     curl -sLO $HDF5_URL/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz
     tar zxf hdf5-$HDF5_VERSION.tar.gz
     ( cd hdf5-$HDF5_VERSION && ./configure --prefix=/usr/local --with-szlib=/usr/local && make && make install )
     tar zcf $LIBRARIES/hdf5-$HDF5_VERSION.tgz /usr/local/lib/* /usr/local/bin/*h5* /usr/local/include/* /usr/local/hdf5*
     rm -rf /usr/local/lib/* /usr/local/bin/*h5* /usr/local/include/* /usr/local/hdf5*
 done
+echo "Done building $HDF5_VERSIONS"
