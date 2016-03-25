b08476096977a6eb00fd6b9d5d0808e0d5da25e4
Matthew Brett <matthew.brett@gmail.com>
2016-03-25 16:17:55 -0700

    Add script for building numexpr

---
+++ build_numexprs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+# Manylinux, openblas version, lex_ver
+source /io/common_vars.sh
+
+if [ -z "$PANDAS_VERSIONS" ]; then
+    NUMEXPR_VERSIONS="1.3 1.3.1 1.4 1.4.2 2.0 2.2.2 2.3 2.3.1 2.4 2.4.6 2.5"
+fi
+
+# Directory to store wheels
+rm_mkdir unfixed_wheels
+
+# Compile wheels
+for PYTHON in ${PYTHON_VERSIONS}; do
+    PIP="$(cpython_path $PYTHON)/bin/pip"
+    for NUMEXPR in ${NUMEXPR_VERSIONS}; do
+        if [ $(lex_ver $PYTHON) -ge $(lex_ver 3.5) ] ; then
+            np_ver=1.9.0
+        else
+            np_ver=1.6.1
+        fi
+        echo "Building numexpr $NUMEXPR for Python $PYTHON"
+        # Put numpy version into the wheelhouse to avoid rebuilding
+        $PIP wheel -f $WHEELHOUSE -f $MANYLINUX_URL -w tmp "numpy==$np_ver"
+        $PIP install -f tmp "numpy==$np_ver"
+        # Add numpy to requirements to avoid upgrading numpy version
+        $PIP wheel -f tmp -w unfixed_wheels "numpy==$np_ver" "numexpr==$NUMEXPR"
+    done
+done
+
+# Bundle external shared libraries into the wheels
+repair_wheelhouse unfixed_wheels $WHEELHOUSE
--- build_openblas_stack.sh
+++ build_openblas_stack.sh
@@ -9,8 +9,10 @@ export NUMPY_VERSIONS=1.10.4
 export SCIPY_VERSIONS=0.17.0
 export SCIKIT_LEARN_VERSIONS=0.17.1
 export PANDAS_VERSIONS=0.18.0
+export NUMEXPR_VERSIONS=2.5
 bash /io/build_openblas.sh
 bash /io/build_numpies.sh
 bash /io/build_scipies.sh
 bash /io/build_sklearns.sh
 bash /io/build_pandas.sh
+bash /io/build_numexprs.sh
