79e5e70652c39a485e50071452219c7f3955fdf6
Matthew Brett <matthew.brett@gmail.com>
2016-03-25 17:58:45 -0700

    Refactor build_stack script

---
+++ build_stack.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Run with:
+#    docker run --rm -v $PWD:/io quay.io/pypa/manylinux1_x86_64 /io/build_stack.sh
+# or something like:
+#    docker run --rm -e PYTHON_VERSIONS=2.7 -v $PWD:/io quay.io/pypa/manylinux1_x86_64 /io/build_stack.sh
+export BLAS_SOURCE="${BLAS_SOURCE:-atlas}"
+export ATLAS_TYPE="${ATLAS_TYPE:-default}"
+export BUILD_SUFFIX="${BUILD_SUFFIX:--$BLAS_SOURCE}"
+export NUMPY_VERSIONS="${NUMPY_VERSIONS:-1.10.4}"
+export SCIPY_VERSIONS="${SCIPY_VERSIONS:-0.17.0}"
+export SCIKIT_LEARN_VERSIONS="${SCIKIT_LEARN_VERSIONS:-0.17.1}"
+export PANDAS_VERSIONS="${PANDAS_VERSIONS:-0.18.0}"
+export NUMEXPR_VERSIONS="${NUMEXPR_VERSIONS:-2.5}"
+if [ "$BLAS_SOURCE" == "openblas" ]; then
+    bash /io/build_openblas.sh
+fi
+bash /io/build_numpies.sh
+bash /io/build_scipies.sh
+bash /io/build_sklearns.sh
+bash /io/build_pandas.sh
+bash /io/build_numexprs.sh
