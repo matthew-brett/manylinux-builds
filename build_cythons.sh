1f0e87238e8e8ebfb17acb4868531d5153d5a3fa
Matthew Brett <matthew.brett@gmail.com>
2016-04-16 16:52:01 -0700

    Add latest Cython to default Cython builds
    [skip ci]

--- build_cythons.sh
+++ build_cythons.sh
@@ -5,7 +5,7 @@
 # or something like:
 #    docker run --rm -e PYTHON_VERSIONS=2.7 -v $PWD:/io quay.io/pypa/manylinux1_x86_64 /io/build_cythons.sh
 # or:
-#    docker run --rm -e CYTON_VERSIONS=0.23.4 -e PYTHON_VERSIONS=2.7 -v $PWD:/io quay.io/pypa/manylinux1_x86_64 /io/build_cythons.sh
+#    docker run --rm -e CYTHON_VERSIONS=0.23.4 -e PYTHON_VERSIONS=2.7 -v $PWD:/io quay.io/pypa/manylinux1_x86_64 /io/build_cythons.sh
 #
 # Then upload pages from ``wheelhouse`` to the manylinux server.
 set -e
@@ -17,7 +17,7 @@ if [ -z $CYTHON_VERSIONS ]; then
     CYTHON_VERSIONS="0.17 0.17.1 0.17.2 0.17.3 0.17.4 0.18 \
         0.19 0.19.1 0.19.2 0.20 0.20.1  0.20.2 \
         0.21 0.21.1 0.21.2 0.22 0.22.1 \
-        0.23 0.23.1 0.23.2 0.23.3 0.23.4 0.23.5"
+        0.23 0.23.1 0.23.2 0.23.3 0.23.4 0.23.5 0.24"
 fi
 
 # Directory to store wheels
