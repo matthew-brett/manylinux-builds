6755f6629a1227bfdccbf6253bf1506b8c6a1aa9
Matthew Brett <matthew.brett@gmail.com>
2016-04-16 18:00:48 -0700

    Add back scipy requirements for scikit-image
    They were doing no harm, really.

--- build_skimages.sh
+++ build_skimages.sh
@@ -24,14 +24,19 @@ for PYTHON in ${PYTHON_VERSIONS}; do
     for SCIKIT_IMAGE in ${SCIKIT_IMAGE_VERSIONS}; do
         if [ $(lex_ver $PYTHON) -ge $(lex_ver 3.5) ] ; then
             NUMPY_VERSION=1.9.1
+            SCIPY_VERSION=0.16.0
+        elif [ $(lex_ver $PYTHON) -ge $(lex_ver 3) ] ; then
+            NUMPY_VERSION=1.7.2
+            SCIPY_VERSION=0.12.0
         else
             NUMPY_VERSION=1.7.2
+            SCIPY_VERSION=0.9.0
         fi
         echo "Building scikit-image $SCIKIT_IMAGE for Python $PYTHON"
         # Put numpy and scipy into the wheelhouse to avoid rebuilding
         $PIP wheel -f $WHEELHOUSE -f $MANYLINUX_URL -w tmp \
             "numpy==$NUMPY_VERSION" "scipy==$SCIPY_VERSION"
-        $PIP install -f tmp "numpy==$NUMPY_VERSION" "scipy"
+        $PIP install -f tmp "numpy==$NUMPY_VERSION" "scipy==$SCIPY_VERSION"
         # Add numpy and scipy  to requirements to avoid upgrading
         $PIP wheel -f tmp -w unfixed_wheels "numpy==$NUMPY_VERSION" \
             "scipy==$SCIPY_VERSION" "scikit-image==$SCIKIT_IMAGE"
