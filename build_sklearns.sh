c7c67d548e5dc774b2c71474d0679eee8b98abb8
Matthew Brett <matthew.brett@gmail.com>
2016-04-21 23:47:01 -0700

    Make scikit-learn compile binary; fix syntax error

--- build_sklearns.sh
+++ build_sklearns.sh
@@ -36,8 +36,12 @@ for PYTHON in ${PYTHON_VERSIONS}; do
             "numpy==$NUMPY_VERSION" "scipy==$SCIPY_VERSION"
         $PIP install -f tmp "numpy==$NUMPY_VERSION" "scipy==$SCIPY_VERSION"
         # Add numpy and scipy  to requirements to avoid upgrading
-        $PIP wheel -f tmp -w unfixed_wheels "numpy==$NUMPY_VERSION" \
-            "scipy==$SCIPY_VERSION" "scikit-learn==$SCIKIT_LEARN"
+        # Prevent download of scikit-learn from pypi
+        $PIP wheel -f tmp -w unfixed_wheels \
+            --no-binary scikit-learn \
+            "numpy==$NUMPY_VERSION" \
+            "scipy==$SCIPY_VERSION" \
+            "scikit-learn==$SCIKIT_LEARN"
     done
 done
 
